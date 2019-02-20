5e31261893225a7bbc84731c9c96d2a405a4bb01
Brea <breajanea@gmail.com>
2019-02-20 18:18:00 -0500

    second commit

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-function playGame(){
 
-    const gameStart = prompt ('Do you want to play a game?');
-    if (gameStart.toLowerCase() === 'yes'){
-    const username = prompt ('What is your name?');
-    console.log(`Hello, ${username}!`);
-    
-     
-    let userPoints = 40;
-    let grantPoints = 10;
-    let userWins = 0;
-    
-    
-    while (grantPoints > 0) {
-    console.log(`${username} has ${userPoints -= Math.floor((Math.random() *3) + 1)} health points left!`);
-    console.log(`Grant the Mighty Chicken has ${grantPoints -= Math.floor((Math.random() *3) + 1)} health points left!`);
-    
-     
-    
-      if (userWins >= 3 || grantPoints <= 0){
-        console.log(`${username} wins!`);
-        break;
-          } else if (userPoints <= 0){
 
-        console.log('Grant wins!');
-        break;
+  function startGame() {
+      let answer = prompt("Do you want to play a gmae?");
+      if (answer.toLowerCase() === "yes") {
+          let userName = prompt("What is your name?");
+          startCombat(userName);
+      } else if (answer.toLowerCase() === "no") {
+          console.log("Bye bye");
+      }
+  }
+
+  function getDamage() {
+      return Math.floor(Math.random() * 5 + 1)
+  }
+
+  function startCombat(name) {
+      let userPoints = 40;
+      let grantPoints = 10;
+      let wins = 0;
+
+      
+      while (grantPoints > 0) {
+          let userMove = prompt("Do you want to attack or quit?");
+          if (userMove === "attack") {
+              console.log(`${name}'s health is now: ${userPoints -= getDamage()}`);
+              console.log(`Grant's Health is now: ${grantPoints -= getDamage()}`);
+              if (userPoints <= 0) {
+                  console.log("Grant wins!");
+                  break;
+              } else if (grantPoints <= 0) {
+                  wins++;
+                  if (wins === 3) {
+                      console.log("You won!");
+                      break;
+                  }
+                  console.log(`${name} wins: ${wins}`);
+                  grantPoints = 10;
+              }
+          } else if (userMove.toLowerCase() === "quit") {
+              console.log("Bye bye");
+              break;
           }
-    
-       else if (grantPoints <= 0) {
-                userWins++;
-                console.log(`${username} wins!`);
-                grantHealth = 10;
-            }
-        }
-    } 
-}
+      }
+  }
+startGame ()
